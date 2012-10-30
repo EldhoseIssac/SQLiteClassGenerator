a9f8a42c7f2797e30ee6304a4b62208d420274df
Eldhose <isletsys@islet21.islet.net>
2012-10-30 09:52:29 +0530

    Initial Commit

---
+++ codegenTst/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  codegenTst
+//
+//  Created by Eldhose on 30/10/12.
+//  Copyright (c) 2012 Islet Systems. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
