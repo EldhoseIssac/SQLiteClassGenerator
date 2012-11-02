764d3a2f9c86258f61c6f5e51af2f4c89e55674c
EldhoseIssac <eldhose.i@gmail.com>
2012-11-02 10:16:52 +0530

    tst

---
+++ SQLiteClassGenerator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  codegenTst
+//
+//  Created by Eldhose on 30/10/12.
+//  Copyright (c) 2012 Islet Systems. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
